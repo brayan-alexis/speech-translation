0b566eede8aebcb23b66289cd9e55fa85e8ec74a
brayan-alexis <brayanzxd@gmail.com>
2023-06-21 21:30:03 -0700

    Set languages

--- speech-translation/Program.cs
+++ speech-translation/Program.cs
@@ -7,58 +7,55 @@ using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using Microsoft.CognitiveServices.Speech.Translation;
 
-namespace Translation
-{
-    class Program
-    {
-        static readonly string SPEECH__SUBSCRIPTION_KEY = "YourSusbcriptionKey";
-        static readonly string SPEECH__REGION = "YourRegion";
+string SPEECH__SUBSCRIPTION_KEY = ""; // Your subscription key
+string SPEECH__REGION = ""; // Your subscription service region
 
-        static void Main(string[] args)
-        {
-            var speechConfig = SpeechConfig.FromSubscription(SPEECH__SUBSCRIPTION_KEY, SPEECH__REGION);
-            var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
-            var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
-            speechRecognizer.Recognizing += (s, e) =>
-            {
-                Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
-            };
-            speechRecognizer.Recognized += (s, e) =>
-            {
-                var result = e.Result;
-                Console.WriteLine($"RECOGNIZED: Text={result.Text}");
-            };
-            speechRecognizer.Canceled += (s, e) =>
-            {
-                Console.WriteLine($"CANCELED: Reason={e.Reason}");
-                if (e.Reason == CancellationReason.Error)
-                {
-                    Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
-                    Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
-                    Console.WriteLine($"CANCELED: Did you update the subscription info?");
-                }
-            };
-            speechRecognizer.SessionStarted += (s, e) =>
-            {
-                Console.WriteLine("\n    Session started event.");
-            };
-            speechRecognizer.SessionStopped += (s, e) =>
-            {
-                Console.WriteLine("\n    Session stopped event.");
-            };
-            speechRecognizer.SpeechStartDetected += (s, e) =>
-            {
-                Console.WriteLine("\n    SpeechStartDetected event.");
-            };
-            speechRecognizer.SpeechEndDetected += (s, e) =>
-            {
-                Console.WriteLine("\n    SpeechEndDetected event.");
-            };
-            Console.WriteLine("Say something...");
-            speechRecognizer.StartContinuousRecognitionAsync().Wait();
-            Console.WriteLine("Press any key to stop");
-            Console.ReadKey();
-            speechRecognizer.StopContinuousRecognitionAsync().Wait();
-        }
+var translationConfig = SpeechTranslationConfig.FromSubscription(SPEECH__SUBSCRIPTION_KEY, SPEECH__REGION);
+var fromLanguage = "es-MX";
+var toLanguages = new List<string> { "en-US", "fr", "de" };
+translationConfig.SpeechRecognitionLanguage = fromLanguage;
+toLanguages.ForEach(translationConfig.AddTargetLanguage);
+
+var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+var speechRecognizer = new SpeechRecognizer(translationConfig, audioConfig);
+
+speechRecognizer.Recognizing += (s, e) =>
+{
+    Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+};
+speechRecognizer.Recognized += (s, e) =>
+{
+    var result = e.Result;
+    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+};
+speechRecognizer.Canceled += (s, e) =>
+{
+    Console.WriteLine($"CANCELED: Reason={e.Reason}");
+    if (e.Reason == CancellationReason.Error)
+    {
+        Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+        Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+        Console.WriteLine($"CANCELED: Did you update the subscription info?");
     }
-}
+};
+speechRecognizer.SessionStarted += (s, e) =>
+{
+    Console.WriteLine("\n    Session started event.");
+};
+speechRecognizer.SessionStopped += (s, e) =>
+{
+    Console.WriteLine("\n    Session stopped event.");
+};
+speechRecognizer.SpeechStartDetected += (s, e) =>
+{
+    Console.WriteLine("\n    SpeechStartDetected event.");
+};
+speechRecognizer.SpeechEndDetected += (s, e) =>
+{
+    Console.WriteLine("\n    SpeechEndDetected event.");
+};
+Console.WriteLine("Say something...");
+speechRecognizer.StartContinuousRecognitionAsync().Wait();
+Console.WriteLine("Press any key to stop");
+Console.ReadKey();
+speechRecognizer.StopContinuousRecognitionAsync().Wait();
