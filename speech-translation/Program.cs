954665e3fa02ad9eaf5d3bfa536d98dc7f057c24
brayan-alexis <brayanzxd@gmail.com>
2023-06-22 23:34:17 -0700

    Working

--- speech-translation/Program.cs
+++ speech-translation/Program.cs
@@ -1,59 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
+﻿using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Translation;
 
-
-
-Console.WriteLine("Starting...");
-await TranslateSpeechToTextAsync();
-
-static async Task TranslateSpeechToTextAsync()
+public class Program
 {
-    string SPEECH__SUBSCRIPTION_KEY = ""; // Your subscription key
-    string SPEECH__REGION = ""; // Your subscription service region
+    static readonly string SubscriptionKey = ""; // Your subscription key
+    static readonly string Region = ""; // Your subscription service region
+    private const string FromLanguage = "es-MX";
+    private static readonly List<string> ToLanguages = new List<string> { "en-US", "fr", "de" };
 
-    var translationConfig = SpeechTranslationConfig.FromSubscription(SPEECH__SUBSCRIPTION_KEY, SPEECH__REGION);
-    var fromLanguage = "es-MX";
-    var toLanguages = new List<string> { "en-US", "fr", "de" };
-    translationConfig.SpeechRecognitionLanguage = fromLanguage;
-    toLanguages.ForEach(translationConfig.AddTargetLanguage);
-
-    using var recognizer = new TranslationRecognizer(translationConfig);
-    Console.WriteLine("Say something in: " + fromLanguage);
-    Console.WriteLine($"We'll translate into '{string.Join("', '", toLanguages)}'.\n");
-    var result = await recognizer.RecognizeOnceAsync();
-    if (result.Reason == ResultReason.TranslatedSpeech)
+    public static async Task Main()
     {
-        Console.WriteLine($"Recognized: \"{result.Text}\":");
-        foreach (var (language, translation) in result.Translations)
+        Console.WriteLine("Starting...");
+        await TranslateSpeechToTextAsync();
+    }
+
+    private static async Task TranslateSpeechToTextAsync()
+    {
+        var translationConfig = SpeechTranslationConfig.FromSubscription(SubscriptionKey, Region);
+        translationConfig.SpeechRecognitionLanguage = FromLanguage;
+        ToLanguages.ForEach(translationConfig.AddTargetLanguage);
+
+        using var recognizer = new TranslationRecognizer(translationConfig);
+
+        Console.WriteLine("Say something in: " + FromLanguage);
+        Console.WriteLine($"We'll translate into '{string.Join("', '", ToLanguages)}'.\n");
+
+        try
         {
-            Console.WriteLine($"Translated into '{language}': {translation}");
+            var result = await recognizer.RecognizeOnceAsync();
+
+            if (result.Reason == ResultReason.TranslatedSpeech)
+            {
+                Console.WriteLine($"Recognized: \"{result.Text}\":");
+                foreach (var (language, translation) in result.Translations)
+                {
+                    Console.WriteLine($"Translated into '{language}': {translation}");
+                }
+            }
+            else if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                Console.WriteLine($"Recognized: {result.Text}");
+            }
+            else if (result.Reason == ResultReason.NoMatch)
+            {
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+            }
+            else if (result.Reason == ResultReason.Canceled)
+            {
+                var cancellation = CancellationDetails.FromResult(result);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-    else if (result.Reason == ResultReason.RecognizedSpeech)
-    {
-        Console.WriteLine($"Recognized: {result.Text}");
-    }
-    else if (result.Reason == ResultReason.NoMatch)
-    {
-        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-    }
-    else if (result.Reason == ResultReason.Canceled)
-    {
-        var cancellation = CancellationDetails.FromResult(result);
-        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-        if (cancellation.Reason == CancellationReason.Error)
-        {
-            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-            Console.WriteLine($"CANCELED: Did you update the subscription info?");
-        }
-    }
-
-
 }
